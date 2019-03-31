4d97381a0a885e4fb352d3720d61c7fc3690092b
sundjly <sundjly@foxmail.com>
2019-04-01 02:38:55 +0800

    fix 修复tsconfig和tslint的错误，规定webpack的查找

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,9 @@ module.exports = {
   entry: {
     index: './lib/index.tsx'
   },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  },
   output: {
     path: path.resolve(__dirname, 'dist/lib'), //绝对路径
     library: 'ICON',
