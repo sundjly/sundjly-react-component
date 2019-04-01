a053216b089ba03265a7c00f23dc75e782bfb7f3
sundjly <sundjly@foxmail.com>
2019-04-02 01:22:36 +0800

    add  webpack.config separate

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   // mode: 'development',
-  mode: 'production',
+  // mode: 'production',
   entry: {
     index: './lib/index.tsx'
   },
@@ -22,24 +22,24 @@ module.exports = {
       }
     ]
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title:'sundjly',
-      template: 'index.html'
-    })
-  ],
-  externals: { // 在依赖中排除第三方包
-    react: {
-      commonjs: 'react',
-      commonjs2: 'react',
-      amd: 'react',
-      root: 'React',
-    },
-    'react-dom': {
-      commonjs: 'react-dom',
-      commonjs2: 'react-dom',
-      amd: 'react-dom',
-      root: 'ReactDOM',
-    },
-  }
+  // plugins: [
+  //   new HtmlWebpackPlugin({
+  //     title:'sundjly',
+  //     template: 'index.html'
+  //   })
+  // ],
+  // externals: { // 在依赖中排除第三方包
+  //   react: {
+  //     commonjs: 'react',
+  //     commonjs2: 'react',
+  //     amd: 'react',
+  //     root: 'React',
+  //   },
+  //   'react-dom': {
+  //     commonjs: 'react-dom',
+  //     commonjs2: 'react-dom',
+  //     amd: 'react-dom',
+  //     root: 'ReactDOM',
+  //   },
+  // }
 };
