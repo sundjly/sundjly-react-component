a4a65cabe9753cd94bc0737765ddd8343a0833f1
sundjly <sundjly@foxmail.com>
2019-04-04 00:37:35 +0800

    feat  添加wechat的icon   运行yarn  start查看效果

--- lib/Icon.tsx
+++ lib/Icon.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
-
+import './icons/wechat.svg';
 interface IconProps {
   name: string;
 }
 
 const Icon: React.FunctionComponent<IconProps> = (props) => {
   return (
-    <span>{props.name}</span>
+    <span>
+      <svg>
+        <use xlinkHref="#wechat"></use>
+      </svg>
+    </span>
   );
 };
 
