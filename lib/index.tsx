deee436478a51835271ac8332392ab2f6899c0ab
sundjly <sundjly@foxmail.com>
2019-04-06 00:03:42 +0800

    feat index.tsx导出默认组件修改

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,33 +1,38 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import Icon from './icon/Icon';
+// import * as React from 'react';
+// import * as ReactDOM from 'react-dom';
 
-const svgClick = (e: React.MouseEvent<SVGElement | SVGUseElement>) => {
-  console.log(e);
-  console.log(e.target);
-  console.log((e.target as SVGUseElement).href); // as进行断言
-};
-// 或者这样声明
-// const svgClick2:React.MouseEventHandler<SVGElement> = (e) => {
-// }
+// 简化
+// import Icon from './icon/Icon';
+// export {Icon};
 
-const svgMouseEnter: React.MouseEventHandler<SVGElement> = () => {
-  console.log('enter');
-};
+export {default as Icon} from './icon/Icon';
 
-const svgMouseLeave: React.MouseEventHandler<SVGElement> = () => {
-  console.log('leave');
-};
-
-ReactDOM.render((<div>
-    <Icon
-      className="icon-wechat"
-      name="wechat"
-      onClick={svgClick}
-      onMouseLeave={svgMouseLeave}
-      onMouseEnter={svgMouseEnter}
-    />
-    <Icon name="qq" onClick={svgClick}/>
-    <Icon name="banana" onClick={svgClick}/>
-  </div>)
-  , document.getElementById('app'));
+// const svgClick = (e: React.MouseEvent<SVGElement | SVGUseElement>) => {
+//   console.log(e);
+//   console.log(e.target);
+//   console.log((e.target as SVGUseElement).href); // as进行断言
+// };
+// // 或者这样声明
+// // const svgClick2:React.MouseEventHandler<SVGElement> = (e) => {
+// // }
+//
+// const svgMouseEnter: React.MouseEventHandler<SVGElement> = () => {
+//   console.log('enter');
+// };
+//
+// const svgMouseLeave: React.MouseEventHandler<SVGElement> = () => {
+//   console.log('leave');
+// };
+//
+// ReactDOM.render((<div>
+//     <Icon
+//       className="icon-wechat"
+//       name="wechat"
+//       onClick={svgClick}
+//       onMouseLeave={svgMouseLeave}
+//       onMouseEnter={svgMouseEnter}
+//     />
+//     <Icon name="qq" onClick={svgClick}/>
+//     <Icon name="banana" onClick={svgClick}/>
+//   </div>)
+//   , document.getElementById('app'));
