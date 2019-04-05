393ad90e6ad4bbd2c35b5a71a65daf658aacb1e9
sundjly <sundjly@foxmail.com>
2019-04-05 16:12:15 +0800

    style  解构精简代码

--- lib/Icon.tsx
+++ lib/Icon.tsx
@@ -2,19 +2,21 @@ import React from 'react';
 import './importAll';
 import './icon.scss';
 import classnames from './helpers/combineClass';
+
 interface IconProps extends React.SVGAttributes<SVGElement> {
   name: string;
 }
 
-const Icon: React.FunctionComponent<IconProps> = (props) => {
-  const {className, ...ohersProp} = props;
-  return (
-    <svg className={classnames('customer-icon', className)}
-         {...ohersProp}
-    >
-      <use xlinkHref={`#${props.name}`}/>
-    </svg>
-  );
-};
+const Icon: React.FunctionComponent<IconProps> =
+  ({className, name, ...ohersProp}) => {
+    return (
+      <svg
+        className={classnames('customer-icon', className)}
+        {...ohersProp}
+      >
+        <use xlinkHref={`#${name}`}/>
+      </svg>
+    );
+  };
 
 export default Icon;
