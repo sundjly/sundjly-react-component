9ed637336a90002a01c2c04c4523e588bef1186c
sundjly <sundjly@foxmail.com>
2019-04-05 22:55:39 +0800

    fix  修复circleci  config的错误

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,7 @@ module.exports = {
   verbose: true,
   clearMocks: false,
   collectCoverage: false,
-  reporters: ['default'],
+  reporters: ['default', "jest-junit"],
 
   collectCoverageFrom:["lib/**/*.{ts,tsx}", "!**/node_modules/**"],
   coverageDirectory:"coverage",
