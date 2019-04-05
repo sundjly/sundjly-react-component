3b8fb19d08d4c6e4df68567981c97adad84b9c4d
sundjly <sundjly@foxmail.com>
2019-04-05 17:02:50 +0800

    test 用snapshots测试icon 功能

---
+++ lib/icon/__tests__/icon.unit.jsx
@@ -0,0 +1,17 @@
+import * as renderer from 'react-test-renderer';
+import React from 'react';
+import Icon from '../Icon';
+import {mount} from 'enzyme';
+
+describe('icon', () => {
+  it('render successfully', () => {
+    const json = renderer.create(<Icon name="qq"/>).toJSON();
+    expect(json).toMatchSnapshot();
+  });
+  it('onClick', () => {
+    const fn = jest.fn();
+    const component = mount(<Icon name="wechat" onClick={fn}/>);
+    component.find('svg').simulate('click');
+    expect(fn).toBeCalled();
+  });
+});
