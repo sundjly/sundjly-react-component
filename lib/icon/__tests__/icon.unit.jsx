b72ca6310123dc094735a30ea51771cd1921dcb1
sundjly <sundjly@foxmail.com>
2019-04-05 17:11:36 +0800

    test enzyme进行点击事件测试

--- lib/icon/__tests__/icon.unit.jsx
+++ lib/icon/__tests__/icon.unit.jsx
@@ -1,14 +1,15 @@
 import * as renderer from 'react-test-renderer';
 import React from 'react';
 import Icon from '../Icon';
-import {mount} from 'enzyme';
+import {mount} from 'enzyme'; // 测试点击事件引入的库
 
-describe('icon', () => {
-  it('render successfully', () => {
+describe('icon组件测试', () => {
+  it('icon render successfully', () => {
     const json = renderer.create(<Icon name="qq"/>).toJSON();
     expect(json).toMatchSnapshot();
   });
-  it('onClick', () => {
+
+  it('icon onClick', () => {
     const fn = jest.fn();
     const component = mount(<Icon name="wechat" onClick={fn}/>);
     component.find('svg').simulate('click');
