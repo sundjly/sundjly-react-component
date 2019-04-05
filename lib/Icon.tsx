0d0f2b4d29f6aa7c7bcdcc3132b538709bdb8734
sundjly <sundjly@foxmail.com>
2019-04-05 12:09:17 +0800

    feat 添加点击事件

--- lib/Icon.tsx
+++ lib/Icon.tsx
@@ -4,11 +4,12 @@ import './icon.scss';
 
 interface IconProps {
   name: string;
+  onClick?: React.MouseEventHandler<SVGElement>
 }
 
 const Icon: React.FunctionComponent<IconProps> = (props) => {
   return (
-    <svg className={'customer-icon'}>
+    <svg className={'customer-icon'} onClick={props.onClick}>
       <use xlinkHref={`#${props.name}`}></use>
     </svg>
   );
--- lib/index.tsx
+++ lib/index.tsx
@@ -1,10 +1,18 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import Icon from './Icon';
+const svgClick = (e:React.MouseEvent<SVGElement| SVGUseElement>) => {
+  console.log(e);
+  console.log(e.target);
+  console.log((e.target as SVGUseElement).href); // as进行断言
+}
+// 或者这样声明
+// const svgClick2:React.MouseEventHandler<SVGElement> = (e) => {
+// }
 
 ReactDOM.render((<div>
-    <Icon name="wechat"/>
-    <Icon name="qq"/>
-    <Icon name="banana"/>
+    <Icon name="wechat" onClick={svgClick}/>
+    <Icon name="qq" onClick={svgClick}/>
+    <Icon name="banana" onClick={svgClick}/>
   </div>)
   , document.getElementById('app'));
