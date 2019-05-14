0256a609a0df5a3825bb7ae58d7a920b6749432e
sundjly <sundljy@gmail.com>
2019-05-14 16:24:22 +0800

    feat 更新 formatClassName 的方法，可以组合以及传递其他

---
+++ lib/helpers/generateCustomClass.ts
@@ -0,0 +1,22 @@
+interface Options {
+	extra: string | undefined;
+}
+
+interface ClassToggles {
+	[key: string]: boolean;
+}
+
+// 解决 class 命名繁琐问题
+const formatClass = (prefix: string) => (name: string | ClassToggles, options?: Options): string => {
+	return Object.entries(name instanceof Object ? name : {[name]: name})
+		.filter(keyValue => keyValue[1] !== false)
+		.map(kv => kv[0])
+		.map(name => [prefix, name]
+			.filter(Boolean)
+			.join('-'))
+		.concat(options && options.extra || [])
+		.join(' ');
+
+};
+
+export {formatClass};
--- lib/utils/generateCustomClass.ts
+++
@@ -1,6 +0,0 @@
-// 解决 class 命名繁琐问题
-const formatClass = (prefix: string) => (name?: string): string => {
-	return [prefix, name].filter(Boolean).join('-');
-};
-
-export {formatClass};
