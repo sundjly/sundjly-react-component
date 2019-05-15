b2f402b869e47e127c921d5a8d47d08092205382
sundjly <sundjly@foxmail.com>
2019-05-16 01:32:07 +0800

    refactor  利用 Layout 组件重构官网

--- examples/example.tsx
+++ examples/example.tsx
@@ -7,38 +7,39 @@ import DialogExample from '../lib/dialog/dialog.example';
 import LayoutExample from '../lib/layout/layout.example';
 import '../lib/index.scss';
 import './example.scss';
+import {Layout, Aside, Content, Header} from '../lib/layout/layout';
 
 ReactDOM.render(
-	<Router>
-		<div className="theme-container">
-			<header className="navbar">
-				<span className="logo">sundjly's Component</span>
-			</header>
-			<div className="main-container">
-				<aside>
-					<p className="header">组件</p>
-					<ul>
-						<li>
-							<Link to="/icon">Icon</Link>
-						</li>
-						<li>
-							<Link to="/button">Button</Link>
-						</li>
-						<li>
-							<Link to="/dialog">Dialog</Link>
-						</li>
-						<li>
-							<Link to={'/layout'}>Layout</Link>
-						</li>
-					</ul>
-				</aside>
-				<main>
-					<Route path="/icon" component={IconExample}/>
-					<Route path="/button" component={ButtonExample}/>
-					<Route path="/dialog" component={DialogExample}/>
-					<Route path="/layout" component={LayoutExample}/>
-				</main>
-			</div>
-		</div>
-	</Router>
-	, document.querySelector('#app'));
+  <Router>
+    <Layout className="theme-container">
+      <Header className="nav-bar">
+        <div className="logo">sundjly's Component</div>
+      </Header>
+      <Layout className="main-container">
+        <Aside className={'main-side'}>
+          <p className="header">组件</p>
+          <ul>
+            <li>
+              <Link to="/icon">Icon</Link>
+            </li>
+            <li>
+              <Link to="/button">Button</Link>
+            </li>
+            <li>
+              <Link to="/dialog">Dialog</Link>
+            </li>
+            <li>
+              <Link to={'/layout'}>Layout</Link>
+            </li>
+          </ul>
+        </Aside>
+        <Content className={'main-content'}>
+          <Route path="/icon" component={IconExample}/>
+          <Route path="/button" component={ButtonExample}/>
+          <Route path="/dialog" component={DialogExample}/>
+          <Route path="/layout" component={LayoutExample}/>
+        </Content>
+      </Layout>
+    </Layout>
+  </Router>
+  , document.querySelector('#app'));
