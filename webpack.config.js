7cb62d9b2cf500c48b0416085a4c040a3da459f9
sundjly <sundjly@foxmail.com>
2019-05-16 17:55:00 +0800

    add  安装 file-loader 解析图片，声明 png 类型（解决 ts 无法识别问题）

--- webpack.config.js
+++ webpack.config.js
@@ -27,6 +27,10 @@ module.exports = {
       {
         test: /\.scss$/,
         loader: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /.(png|svg|jpg|gif|jpeg)$/,
+        use: ['file-loader']
       }
     ]
   },
