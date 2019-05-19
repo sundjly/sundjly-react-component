dc990e142c84129ebfb812c24f791cea26c758b8
sundjly <sundljy@gmail.com>
2019-05-19 13:38:34 +0800

    feat  Icon 组件添加新的 API

--- lib/icon/Icon.tsx
+++ lib/icon/Icon.tsx
@@ -1,22 +1,44 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './importAll';
 import './style/icon.scss';
 import classnames from '../helpers/combineClass';
+import {log} from 'util';
 
 interface IconProps extends React.SVGAttributes<SVGElement> {
-  name: string;
+	name: string;
+	scriptUrl?: string;
 }
 
-const Icon: React.FunctionComponent<IconProps> =
-  ({className, name, ...ohersProp}) => {
-    return (
-      <svg
-        className={classnames('customer-icon', className)}
-        {...ohersProp}
-      >
-        <use xlinkHref={`#${name}`}/>
-      </svg>
-    );
-  };
-
+const customCache = new Set<string>();
+const Icon: React.FunctionComponent<IconProps> = (props) => {
+	const {className, name, scriptUrl, ...ohersProp} = props;
+	useEffect(() => {
+		log('111');
+		if (typeof document !== 'undefined'
+			&& typeof window !== 'undefined'
+			&& typeof document.createElement === 'function'
+			&& typeof scriptUrl === 'string'
+			&& scriptUrl.length
+			&& !customCache.has(scriptUrl)
+		) {
+			const script: HTMLElement = document.createElement('script');
+			script.setAttribute('src', scriptUrl);
+			script.setAttribute('data-namespance', scriptUrl);
+			customCache.add(scriptUrl);
+			document.body.appendChild(script);
+			log('script')
+		}
+	}, []);
+	return (
+		<svg
+			className={classnames('customer-icon', className)}
+			{...ohersProp}
+		>
+			<use xlinkHref={`#${name}`}/>
+		</svg>
+	);
+};
+Icon.defaultProps={
+	scriptUrl:'//at.alicdn.com/t/font_1198870_a7vz2zkwc3n.js'
+}
 export default Icon;
